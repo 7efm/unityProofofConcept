edcfdfdeaf6fc817a30a31d764002bf0ad6eee59
7efm <38299622+7efm@users.noreply.github.com>
2019-12-04 12:55:46 -0800

    Started Dialogue

---
+++ Proof of concept 2/Assets/Script/npcD.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class npcD : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public Text dialogText;
+    public string dialog;
+    public bool dialogOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            Debug.Log("Player in range");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player not in range");
+        }
+    }
+}
+//timestamp 15:00
