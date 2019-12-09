c1e5a2e4ed8216e56fb44a857bc5b74e2f82e0c3
7efm <38299622+7efm@users.noreply.github.com>
2019-12-09 12:48:59 -0800

    Added Hint System 12.9

---
+++ Proof of concept 2/Assets/Script/Hints.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hints : MonoBehaviour
+{
+    public GameObject hintBox;
+    public Text hintText;
+    public string hint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hintText.text = "Press space to talk to people when you are close";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (hintBox.activeInHierarchy)
+            {
+                hintBox.SetActive(false);
+
+            }
+            
+            }
+        }
+
+}
+
+//timestamp 15:00
