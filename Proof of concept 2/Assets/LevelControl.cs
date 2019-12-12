3244f704377e7216cdfe74358dfc7826dab5f938
7efm <38299622+7efm@users.noreply.github.com>
2019-12-11 21:32:44 -0800

    12.11.19

---
+++ Proof of concept 2/Assets/LevelControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelControl : MonoBehaviour
+{
+    public int index;
+    public string levelName;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) {
+            SceneManager.LoadScene(index);
+        }
+    }
+    public void EnterLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+    // Update is called once per frame
+
+    void Update()
+    {
+        
+    }
+}
--- Proof of concept 2/Assets/Script/npcD.cs
+++ Proof of concept 2/Assets/Script/npcD.cs
@@ -8,13 +8,14 @@ public class npcD : MonoBehaviour
     public GameObject dialogBox;
     public GameObject alertBox;
     public GameObject portrait;
-    public Image image;
+    public Sprite image;
     public Text dialogText;
     public string dialog;
     public bool playerInRange;
     // Start is called before the first frame update
     void Start()
     {
+        portrait.GetComponent<Image>().sprite = image;
     }
 
     // Update is called once per frame
