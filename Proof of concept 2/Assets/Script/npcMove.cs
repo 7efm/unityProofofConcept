5f30c5a68d7ec5324b131da88e768eca1f219f55
7efm <38299622+7efm@users.noreply.github.com>
2020-01-29 12:58:07 -0800

    1.29.20

---
+++ Proof of concept 2/Assets/Script/npcMove.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class npcMove : MonoBehaviour
+{
+    // Public values
+    public float Speed = .3f;
+    // Private values
+    private Vector2 initialPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * Speed * Time.deltaTime);
+        Debug.Log("Update is running");
+        Debug.Log(Speed);
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            Debug.Log("inrange");
+            Speed = 0f;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            Debug.Log("notinrange");
+
+        }
+    }
+}
